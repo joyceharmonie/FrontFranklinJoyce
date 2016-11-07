9f531923c43a56ff12a8c96fafee8ecf0be23752
joyceharmonie <joyce.goutane@gmail.com>
2016-11-07 17:10:49 +0100

    avancement login

--- src/app/features/login/login.controller.js
+++ src/app/features/login/login.controller.js
@@ -6,9 +6,30 @@
     .controller('LoginController', LoginController);
 
   /** @ngInject */
-  function LoginController($timeout, webDevTec, toastr) {
+  function LoginController(AuthenticateService) {
     var vm = this;
+    console.log(AuthenticateService);
 
+    vm.user = {};
+    vm.user.username = "test@gmail.com";
+    vm.user.password = "password";
+
+    vm.send = send;
+
+
+    /* functions */
+    function send() {
+      console.log("send");
+      AuthenticateService.check();
+      AuthenticateService.login(vm.user).then(function(response) {
+        if (response.status == 200) {
+          console.log("login success");
+        }
+        else {
+          console.log("unsuccessful login");
+        }
+      })
+    }
 
   }
 })();
